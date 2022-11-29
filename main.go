81cf31e294c1d4d58ec651039efff3ee75a709dc
wangjia <wangjia@patsnap.com>
2022-11-29 13:28:14 +0800

    fix encoding

--- main.go
+++ main.go
@@ -1,30 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func main() {
 	url := "https://www.thepaper.cn/"
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("fetch url error:%v", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code:%v", resp.StatusCode)
-		return
-	}
-	body, err := io.ReadAll(resp.Body)
+	body, err := Fetch(url)
 
 	if err != nil {
 		fmt.Println("read content failed:%v", err)
 		return
 	}
-	fmt.Println("body:", string(body))
+	numLinks := strings.Count(string(body), "<a")
+	fmt.Printf("homepage has %d links!\n", numLinks)
 
+	exist := strings.Contains(string(body), "疫情")
+	fmt.Printf("是否存在疫情:%v\n", exist)
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code:%d", resp.StatusCode)
+	}
+	reader := bufio.NewReader(resp.Body)
+	determinEncoding := DeterminEncoding(reader)
+	// 将 HTML 文本从特定编码转换为 UTF-8 编码
+	newReader := transform.NewReader(reader, determinEncoding.NewDecoder())
+
+	return io.ReadAll(newReader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		fmt.Println("fetch error:%v", err)
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
